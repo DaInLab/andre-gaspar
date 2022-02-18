a0ebfdf42eb1c0b38291feb053b69083f18fb3ea
André Gaspar <and_lrg@hotmail.com>
2022-02-17 21:26:21 -0300

    Updating ex 2

--- src/exercicio_2.r
+++ src/exercicio_2.r
@@ -9,8 +9,6 @@ worksheet <- read_excel("./dados/exercicio2.xls")
 
 f_table <- as.data.frame(table(worksheet))
 
-print(paste("Tabela de frequência: ", f_table))
-
 mean        <- mean(worksheet$'Casas')
 median      <- median(worksheet$'Casas')
 varance     <- var(worksheet$'Casas')
@@ -20,7 +18,7 @@ moda        <- moda(worksheet$'Casas')
 headerCalc <- c ("Media", "Mediana", "Moda", "Variancia", "Desvio Padrao")
 calc <- c (mean, median, moda, varance, sd)
 
-png(filename = "../graficos/exerc2.png", width = 1000, height = 1000)
-barplot(calc, main="Planilha 2 - Ex. 2", xlab="Valores Estátisticos", ylab="Casas", 
+png(filename = "../graficos/exercicio_2.png", width = 1000, height = 1000)
+barplot(calc, main="Exercicio 2", xlab="Valores Estátisticos", ylab="Casas", 
         names.arg = headerCalc, ylim=c(0, 700), cex.names = 0.8, xaxs = "i", col=rainbow(5))
 dev.off()
