9cb74041c124808ab6c0af45c2f1b1a40c1ea51f
André Gaspar <and_lrg@hotmail.com>
2022-02-17 22:01:09 -0300

    Exercicio 9

---
+++ src/exercicio_9.r
@@ -0,0 +1,22 @@
+library(readxl)
+library(dplyr)
+
+data <- read_excel("./dados/exercicio9.xls")
+sals <- data$'Salários'
+
+png(filename = "../graficos/exercicio9.png", width = 1000, height = 1000)
+
+hist(sals, main="Exercício 9", xlab="Salarios", ylab="Frequencia", col=rainbow(5))
+
+dev.off()
+
+table <- data.frame(t(table(sals)))[,-1]
+table$sals <- as.numeric(levels(table$sals))
+
+table <- table %>% 
+  mutate(
+    Fr = 100*Freq/sum(Freq),
+    Fac = cumsum(Freq),
+    Xi.Fi = sals*Freq
+  )
+table
